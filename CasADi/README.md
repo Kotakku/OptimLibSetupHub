d2dd94d6a03d0a6a12a708598466d6f29ba03b5c
Kotakku <kotakkucu@gmail.com>
2023-09-05 01:13:18 +0900

    add CasADi

---
+++ CasADi/README.md
@@ -0,0 +1,11 @@
+# CasADi
+
+最適制御問題や非線形最適化問題を解くための自動微分と数値最適化を提供するフレームワーク
+
+## Repository
+https://github.com/casadi/casadi
+
+## Install
+```
+bash install_casadi.sh
+```
---
+++ CasADi/install_casadi.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+sudo apt install -y git cmake gcc g++ gfortran pkg-config liblapack-dev pkg-config coinor-libipopt-dev --install-recommends
+git clone https://github.com/casadi/casadi.git casadi
+echo "export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH:$HOME/local/:/opt/hpipm/cmake"" >> ~/.bashrc
+source ~/.bashrc
+cd casadi
+mkdir build
+cd build
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_IPOPT=ON -DWITH_QPOASES=ON 
+make -j4
+sudo make install
+
+# ## 一発で入れ用とするとなぜかHPIPMのビルドでコケる
+# # HPIPMのためにBLASFEOが必要
+# cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_BLASFEO=ON -DWITH_BUILD_BLASFEO=ON
+# make -j4
+# sudo make install
+
+# # IPOPTとHPIPMを追加
+# cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_IPOPT=ON -DWITH_HPIPM=ON -DWITH_BLASFEO=ON -DWITH_BUILD_HPIPM=ON -DWITH_BUILD_BLASFEO=ON -DWITH_LAPACK=ON -DWITH_QPOASES=ON 
+# make -j4
+# sudo make install
